f276c503d18cd6a298daab24d16b245f7fdbb9cf
Jamie Davenport <jdodger_@hotmail.co.uk>
2020-03-05 16:49:10 +0000

    Made loop constant and moved delay to static var

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import * as https from "https";
 import { IncomingMessage } from "http";
 /*
     TODOS:
-        1. Fetch a price from API
-        2. Run regex to get the price from that API call
+        1. [DONE] Fetch a price from API
+        2. [DONE] Run regex to get the price from that API call
         3. Store the resulting price in a local file
         4. Adapt above to loop over list of stocks
+        5. Make regex work for each stock by substituting stock name
+        6. Make process interuptable
 */
 
 // API Link:
@@ -16,9 +18,13 @@ class StockFetcher {
     //Properties
     private static readonly URL = "https://www.google.com/async/finance_wholepage_price_updates?ei=AkRUXrqGAZKP8gLipYzoDQ&yv=3&async=mids:%2Fm%2F0ckhqlx,currencies:,_fmt:jspb";
     private static readonly REGEX = /(?<=("(TSLA)",))"(.*?)"/;
+    private static readonly FREQUENCY = 10;
+
+    private running: boolean;
 
     // Constructor
     constructor() {
+        this.running = true;
         this.StartLooping()
             .then(() => {
                 console.log("done");
@@ -32,15 +38,16 @@ class StockFetcher {
 
     private async StartLooping(): Promise<void> {
         try {
-            // Loop 10 times to fetch 10 prices
-            for (let i = 0; i < 10; i++) {
+            // Also need a stopping mechanism
+            while (this.running) {
                 const result = await this.GetPrice();
                 console.log(result);
-                await this.Delay(20);
+                await this.Delay(StockFetcher.FREQUENCY);
             }
             Promise.resolve();
         } catch (e) {
             console.log("An error occurred, error: " + e);
+            // Don't necessarily need to reject here, can handle the error and try to continue
             Promise.reject(e);
         }
     }
